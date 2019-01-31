927a243c53a7ecbda282d206bc186e0ef8adab8a
Dan Zaratsian <dtz001@gmail.com>
2019-01-31 16:39:41 -0500

    Update and rename setup_container_registry.sh to 02_setup_container_registry.sh

--- kafka_simulator_threaded/02_setup_container_registry.sh
+++ kafka_simulator_threaded/02_setup_container_registry.sh
@@ -6,15 +6,17 @@
 #
 ######################################################################
 
+# Variables
+project_id=ml-healthcare-poc-201901 
+container_name=kafka_simulator_threaded
+
 
 # Authenticate - configure Docker to use the gcloud
 gcloud auth configure-docker
 
 
 # Tag the image with a registry name
-project_id=ml-healthcare-poc-201901 
-container_name=kafka_simulator_threaded
-docker tag kafka_simulator_threaded gcr.io/$project_id/$container_name
+docker tag $container_name gcr.io/$project_id/$container_name
 
 
 # Push the image to Container Registry
@@ -22,9 +24,8 @@ docker push gcr.io/$project_id/$container_name
 
 
 # Pull the image from Conainer Registry
-project_id=ml-healthcare-poc-201901
-container_name=kafka_simulator_threaded
 #docker pull gcr.io/$project_id/$container_name
+# Run the image from Conainer Registry
 #docker run gcr.io/$project_id/$container_name
 
 
