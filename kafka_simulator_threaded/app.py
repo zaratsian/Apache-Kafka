1e16b7740528d161eb092cf0a4a94ddf910c7ee6
Dan Zaratsian <dtz001@gmail.com>
2019-02-28 12:00:32 -0500

    Update app.py

--- kafka_simulator_threaded/app.py
+++ kafka_simulator_threaded/app.py
@@ -3,8 +3,7 @@
 #
 #   Data Simulator for Apache Kafka
 #
-#   USAGE:
-#   simulate_to_kafka.py --bootstrap_servers localhost:9092 --kafka_topic dztopic1 --time_delay 1 --send_to_kafka 0
+#   Deployed as a container (scaled with Kubernetes, GKE)
 #
 #   Test with python 3.7
 #   https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
@@ -13,16 +12,7 @@
 #   pip install kafka-python
 #
 #####################################################################
-'''
-./params.py
 
-bootstrap_servers   = 'kafka-instance-1-vm:9092'
-send_to_kafka       = 0
-kafka_topic         = 'eye_tracking'
-time_delay          = 1
-number_of_threads   = 5
-
-'''
 
 from kafka import KafkaProducer
 import json
@@ -32,7 +22,6 @@ import random
 import argparse
 import threading
 import params
-
 
 
 def simulate_payload():
@@ -51,7 +40,7 @@ def simulate_payload():
 
 def initialize_kafka_producer(params):
     try:
-        producer = KafkaProducer(bootstrap_servers=params.bootstrap_servers, value_serializer=lambda v: json.dumps(v).encode('utf-8'))     # JSON-based Producer
+        producer = KafkaProducer(bootstrap_servers=params['bootstrap_servers'], value_serializer=lambda v: json.dumps(v).encode('utf-8'))     # JSON-based Producer
     except Exception as e:
         print('[ EXCEPTION ] Could not connect to Kafka bootstrap server - {}'.format(e))
     
@@ -66,24 +55,32 @@ def publish_kafka_event(params, producer, counter):
         payload = simulate_payload()
         payload['counter'] = counter
         
-        if params.send_to_kafka==1:
+        if params['send_to_kafka']==1:
             try:
-                producer.send(params.kafka_topic, value=payload)   # JSON-based kafka commit
+                producer.send(params['kafka_topic'], value=payload)   # JSON-based kafka commit
             except Exception as e:
                 print('[ EXCEPTION ] Failure sending JSON payload to Kafka producer - {}'.format(e))
         else:
             print('{}\n'.format(payload))
         
-        time.sleep(params.time_delay)
+        time.sleep(params['time_delay'])
 
 
 
 if __name__ == "__main__":
     
+    params = {
+        'bootstrap_servers'   : '35.184.69.2:9092',
+        'send_to_kafka'       : 1,
+        'kafka_topic'         : 'eye_tracking',
+        'time_delay'          : 5,
+        'number_of_threads'   : 100
+    }
+    
     producer = initialize_kafka_producer(params)
     
     threads = []
-    for i in range(params.number_of_threads):
+    for i in range(params['number_of_threads']):
         threads.append(threading.Thread(target=publish_kafka_event, args=(params, producer, i,)))
     
     for thread in threads:
